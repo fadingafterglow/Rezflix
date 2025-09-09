97d4f9ea5af408b669aecb052a0c4ab184e3c739
Mykola Prybysh <m.prybysh@ukma.edu.ua>
2025-09-09 22:33:24 +0300

    Initial commit

---
+++ src/main/java/ua/edu/ukma/springers/rezflix/RezflixApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.ukma.springers.rezflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RezflixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/edu/ukma/springers/rezflix/RezflixApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.ukma.springers.rezflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
