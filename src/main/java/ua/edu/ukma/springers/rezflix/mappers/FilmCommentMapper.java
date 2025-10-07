c377e03cca716137dd7b174fefd1f7c4875f40a3
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-10-07 23:09:21 +0300

    Add FilmCommentMapper

---
+++ src/main/java/ua/edu/ukma/springers/rezflix/mappers/FilmCommentMapper.java
@@ -0,0 +1,15 @@
+package ua.edu.ukma.springers.rezflix.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import ua.edu.ukma.springers.rezflix.controllers.rest.model.CommentDto;
+import ua.edu.ukma.springers.rezflix.controllers.rest.model.CommentListDto;
+import ua.edu.ukma.springers.rezflix.domain.entities.FilmCommentEntity;
+
+@Mapper(config = MapperConfiguration.class)
+public interface FilmCommentMapper extends IResponseMapper<FilmCommentEntity, CommentDto>, IListResponseMapper<FilmCommentEntity, CommentListDto> {
+    @Override
+    @Mapping(target = "author", ignore = true)
+    @Mapping(target = "authorRating", ignore = true)
+    CommentDto toResponse(FilmCommentEntity entity);
+}
