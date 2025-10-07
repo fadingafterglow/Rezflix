a65e15768c3dcb52ddc1f315522df380f96c1876
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-10-07 23:09:07 +0300

    Add FilmCommentRepository

---
+++ src/main/java/ua/edu/ukma/springers/rezflix/repositories/FilmCommentRepository.java
@@ -0,0 +1,6 @@
+package ua.edu.ukma.springers.rezflix.repositories;
+
+import ua.edu.ukma.springers.rezflix.domain.entities.FilmCommentEntity;
+
+public interface FilmCommentRepository extends IRepository<FilmCommentEntity, Integer> {
+}
