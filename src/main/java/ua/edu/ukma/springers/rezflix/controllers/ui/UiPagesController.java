a57f4f5ecc2face70cb83d99f2d9f6c47331114d
andre <andre.chrn@gmail.com>
2025-11-04 21:26:28 +0200

    profile.html correction

--- src/main/java/ua/edu/ukma/springers/rezflix/controllers/ui/UiPagesController.java
+++ src/main/java/ua/edu/ukma/springers/rezflix/controllers/ui/UiPagesController.java
@@ -1,16 +1,21 @@
 package ua.edu.ukma.springers.rezflix.controllers.ui;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ua.edu.ukma.springers.rezflix.services.FilmService;
 import ua.edu.ukma.springers.rezflix.services.UserService;
+import ua.edu.ukma.springers.rezflix.controllers.rest.model.UpsertFilmDto;
 
 @Controller
 @RequiredArgsConstructor
 public class UiPagesController {
 
     private final UserService userService;
+    private final FilmService filmService;
 
     @GetMapping("/login")
     public String getLoginPage() {
@@ -19,6 +24,21 @@ public class UiPagesController {
 
     @GetMapping("/profile")
     public String getProfilePage(Model model) {
+        model.addAttribute("user", userService.getCurrentUserInfo());
+        return "profile";
+    }
+
+    @PostMapping("/profile")
+    @PreAuthorize("hasRole('SUPER_ADMIN')")
+    public String createFilm(UpsertFilmDto filmDto, Model model) {
+
+        try {
+            filmService.create(filmDto);
+            model.addAttribute("filmAddedSuccess", true);
+        } catch (Exception e) {
+            model.addAttribute("filmAddedError", "Помилка додавання: " + e.getMessage());
+        }
+
         model.addAttribute("user", userService.getCurrentUserInfo());
         return "profile";
     }
