9ea33bb97588b91fdc116ca2e5c0daf5d938ee20
Oleksandr Tkachenko <tasitwork@gmail.com>
2025-11-19 03:32:31 +0200

    Add logging aspect

---
+++ src/main/java/ua/edu/ukma/springers/rezflix/aspects/LogServiceExceptionAspect.java
@@ -0,0 +1,21 @@
+package ua.edu.ukma.springers.rezflix.aspects;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LogServiceExceptionAspect {
+    @Pointcut("execution(* ua.edu.ukma.springers.rezflix.services..*(..))")
+    public void serviceMethod() {}
+
+    @AfterThrowing(pointcut = "serviceMethod()", throwing = "ex")
+    public void logException(JoinPoint joinPoint, Throwable ex) {
+        log.error("Exception in {}: {}", joinPoint.getSignature().toShortString(), ex.getMessage());
+    }
+}
