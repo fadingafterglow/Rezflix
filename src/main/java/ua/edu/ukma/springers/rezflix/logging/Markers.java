e7aadf972d45993b2b7cdc658777fd534b544ec0
Mykola Prybysh <m.prybysh@ukma.edu.ua>
2025-11-30 01:49:39 +0200

    Remove UDP appender

--- src/main/java/ua/edu/ukma/springers/rezflix/logging/Markers.java
+++ src/main/java/ua/edu/ukma/springers/rezflix/logging/Markers.java
@@ -1,9 +1,11 @@
 package ua.edu.ukma.springers.rezflix.logging;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 
-public interface Markers {
-    Marker LOCAL = MarkerFactory.getMarker("LOCAL");
-    Marker EXCEPTION = MarkerFactory.getMarker("EXCEPTION");
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Markers {
+    public static final Marker EXCEPTION = MarkerFactory.getMarker("EXCEPTION");
 }
