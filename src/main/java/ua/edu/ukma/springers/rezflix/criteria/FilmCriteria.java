78ab4ebd75bd455dd23e78046a041f98c246a352
Mykola Prybysh <m.prybysh@ukma.edu.ua>
2025-12-01 15:09:52 +0200

    Add rated by user film criterias

--- src/main/java/ua/edu/ukma/springers/rezflix/criteria/FilmCriteria.java
+++ src/main/java/ua/edu/ukma/springers/rezflix/criteria/FilmCriteria.java
@@ -1,14 +1,10 @@
 package ua.edu.ukma.springers.rezflix.criteria;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.*;
 import ua.edu.ukma.criteria.core.Criteria;
 import ua.edu.ukma.criteria.core.PredicatesBuilder;
-import ua.edu.ukma.springers.rezflix.domain.entities.FilmEntity;
+import ua.edu.ukma.springers.rezflix.domain.entities.*;
 import ua.edu.ukma.springers.rezflix.controllers.rest.model.FilmCriteriaDto;
-import ua.edu.ukma.springers.rezflix.domain.entities.FilmEntity_;
 
 import java.util.List;
 
@@ -20,9 +16,33 @@ public class FilmCriteria extends Criteria<FilmEntity, FilmCriteriaDto> {
 
     @Override
     protected <R> List<Predicate> formPredicates(Root<FilmEntity> root, CriteriaQuery<R> query, CriteriaBuilder cb) {
-        return new PredicatesBuilder<>(root, cb)
+        List<Predicate> predicates = new PredicatesBuilder<>(root, cb)
                 .like(values.getQuery(), FilmEntity_.title, FilmEntity_.description)
                 .getPredicates();
+
+        if (values.getRatedByUser() != null) {
+            Predicate predicate = cb.exists(ratingSubquery(values.getRatedByUser(), root, query, cb));
+            predicates.add(predicate);
+        }
+        if (values.getNotRatedByUser() != null) {
+            Predicate predicate = cb.not(cb.exists(ratingSubquery(values.getNotRatedByUser(), root, query, cb)));
+            predicates.add(predicate);
+        }
+
+        return predicates;
+    }
+
+    private <R> Subquery<FilmRatingEntity> ratingSubquery(int userId, Root<FilmEntity> root, CriteriaQuery<R> query, CriteriaBuilder cb) {
+        Subquery<FilmRatingEntity> subquery = query.subquery(FilmRatingEntity.class);
+        Root<FilmRatingEntity> subRoot = subquery.from(FilmRatingEntity.class);
+        subquery.select(subRoot);
+        subquery.where(
+            cb.and(
+                cb.equal(subRoot.get(FilmRatingEntity_.film).get(FilmEntity_.id), root.get(FilmEntity_.id)),
+                cb.equal(subRoot.get(FilmRatingEntity_.user).get(UserEntity_.id), userId)
+            )
+        );
+        return subquery;
     }
 
 }
