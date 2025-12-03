9a02ffe35f9a72dfd6d770f9d86cb58c1b24ec11
Mykola Prybysh <m.prybysh@ukma.edu.ua>
2025-12-03 10:39:31 +0200

    Fix WatchRoomStateDto serialization

--- src/main/java/ua/edu/ukma/springers/rezflix/controllers/websocket/model/WatchRoomStateDto.java
+++ src/main/java/ua/edu/ukma/springers/rezflix/controllers/websocket/model/WatchRoomStateDto.java
@@ -1,9 +1,11 @@
 package ua.edu.ukma.springers.rezflix.controllers.websocket.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class WatchRoomStateDto {
+    @JsonProperty("paused")
     private final boolean isPaused;
     private final long episodePositionMs;
 }
