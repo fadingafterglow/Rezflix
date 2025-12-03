3326ef9a2cd1ab1c81eb251d2e10bc67bf7630b8
Mykola Prybysh <m.prybysh@ukma.edu.ua>
2025-12-03 05:05:58 +0200

    Simplify cache config

--- src/main/java/ua/edu/ukma/springers/rezflix/configuration/CacheConfiguration.java
+++ src/main/java/ua/edu/ukma/springers/rezflix/configuration/CacheConfiguration.java
@@ -1,21 +1,8 @@
 package ua.edu.ukma.springers.rezflix.configuration;
 
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 @EnableCaching
-public class CacheConfiguration {
-
-    @Bean
-    public CacheManager cacheManager() {
-        ConcurrentMapCacheManager manager = new ConcurrentMapCacheManager();
-        manager.setCacheNames(List.of("film", "filmRating", "filmComment", "filmDubbing"));
-        return manager;
-    }
-}
+public class CacheConfiguration {}
